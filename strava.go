9a62044a361b74eb916be95a8a74d8ee55cbaad9
John <29466887+ockendenjo@users.noreply.github.com>
2026-02-09 14:47:08 +0000

    Add missing properties to activities struct

--- strava.go
+++ strava.go
@@ -187,6 +187,8 @@ type Activity struct {
 	ID        int64       `json:"id"`
 	Name      string      `json:"name"`
 	Map       PolylineMap `json:"map"`
+	SportType string      `json:"sport_type"`
+	GearID    string      `json:"gear_id"`
 	StartDate string      `json:"start_date"`
 }
 
