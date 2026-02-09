cf9081e33e25f987ea10a4c418d0ee09f29b3242
John <29466887+ockendenjo@users.noreply.github.com>
2026-02-09 15:19:18 +0000

    Add types for webhook events

---
+++ event.go
@@ -0,0 +1,18 @@
+package strava
+
+type WebhookEvent struct {
+	ObjectType     string          `json:"object_type"`
+	ObjectID       int64           `json:"object_id"`
+	AspectType     string          `json:"aspect_type"`
+	Updates        *WebhookUpdates `json:"updates"`
+	OwnerID        int64           `json:"owner_id"`
+	SubscriptionID int             `json:"subscription_id"`
+	EventTime      int64           `json:"event_time"`
+}
+
+type WebhookUpdates struct {
+	Title      *string `json:"title"`
+	Type       *string `json:"type"`
+	Private    *bool   `json:"private"`
+	Authorized *bool   `json:"authorized"`
+}
